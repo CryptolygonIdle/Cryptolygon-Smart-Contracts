19fb0d99f74dc2804ecf338d212bf3f5966706ac
François Liard <lifra05@yahoo.fr>
2024-02-08 16:52:53 +0100

    Added internal polygon purchase function

--- contracts/CryptolygonIdleV1.sol
+++ contracts/CryptolygonIdleV1.sol
@@ -251,6 +251,39 @@ contract CryptolygonIdleV1 is ICryptolygonIdleV1, Initializable, PausableUpgrade
      * - The player must have enough lines to level up the polygon.
      */
     function _levelUpPolygon(uint256 polygonId, uint256 amount) internal {
+        // Check if the polygon can be leveled up
+        if (polygonId == 0 || amount == 0) {
+            revert PolygonLevelUpNotAllowed(polygonId, amount);
+        }
+
+        PlayerDataV1 memory playerData = playersData[msg.sender];
+
+        // Check if the player has unlocked the previous polygon
+        if (polygonId > 1 && playerData.levelOfPolygons[polygonId - 1] == 0) {
+            revert PolygonLevelUpNotAllowed(polygonId, amount);
+        }
+
+        // Compute the cost of leveling up the polygon
+        BigNumber memory cost = BigNumbers.init(0, false);
+
+        for (uint256 i = 0; i < amount; i++) {
+            cost = cost.add(BigNumbers.init(polygonId**polygonId, false).mul(BigNumbers.init(playerData.levelOfPolygons[polygonId], false)));
+        }
+
+        // Check if the player has enough lines to level up the polygon
+        if (playerData.linesLastUpdate.lt(cost)) {
+            revert NotEnoughLinesToLevelUp(polygonId, amount);
+        }
+
+        // Consume the lines
+        playersData[msg.sender].linesLastUpdate = playerData.linesLastUpdate.sub(cost);
+
+        // Level up the polygon
+        playersData[msg.sender].levelOfPolygons[polygonId] = playerData.levelOfPolygons[polygonId] + (amount);
+
+        // Emit the PolygonLeveledUp event
+        emit PolygonLeveledUp(msg.sender, polygonId, amount);
+
     }
 
     /**
