5ec705883af2681f08aa04f7147ffd99b323aaa3
François Liard <lifra05@yahoo.fr>
2024-02-08 13:43:51 +0100

    Added IERC20, interface functions mockup

--- contracts/CryptolygonIdleV1.sol
+++ contracts/CryptolygonIdleV1.sol
@@ -8,13 +8,17 @@ import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 /// @custom:security-contact
 contract CryptolygonIdleV1 is ICryptolygonIdleV1, Initializable, PausableUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
     using BigNumber for BigNumber;
 
     mapping (address => PlayerDataV1) public playersData;
 
-    address constant public CIRCLE_ADDRESS = 0x
+    IERC20 constant public CIRCLE = IERC20(0x0);
+
+    // Admin functions
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -40,4 +44,84 @@ contract CryptolygonIdleV1 is ICryptolygonIdleV1, Initializable, PausableUpgrade
         onlyOwner
         override
     {}
+
+    // Game external functions
+
+    /**
+     * @inheritdoc ICryptolygonIdleV1
+     */
+    function levelUpPolygons(uint256[] calldata polygonIds, uint256[] calldata amounts) external returns() {
+        require(polygonIds.length == amounts.length, InvalidArguments);
+        require(polygonIds.length > 0, InvalidArguments);
+    }
+
+    /**
+     * @inheritdoc ICryptolygonIdleV1
+     */
+    function buyUpgrades(uint256[] calldata upgradeIds, uint256[] calldata amounts) external returns() {
+        require(upgradeIds.length == amounts.length, InvalidArguments);
+        require(upgradeIds.length > 0, InvalidArguments);
+    }
+
+    /**
+     * @inheritdoc ICryptolygonIdleV1
+     */
+    function buyAscensionPerks(uint256[] calldata perkIds, uint256[] calldata amounts) external returns() {
+        require(perkIds.length == amounts.length, InvalidArguments);
+        require(perkIds.length > 0, InvalidArguments);
+    }
+
+    /**
+     * @inheritdoc ICryptolygonIdleV1
+     */
+    function ascend() external receive() {
+    }
+
+    /**
+     * @inheritdoc ICryptolygonIdleV1
+     */
+    function tip() external payable() {
+        payable(owner()).transfer(msg.value);
+    }
+
+    // Public functions
+
+    /**
+     * @inheritdoc ICryptolygonIdleV1
+     */
+    function getPlayerLinesPerSecond(address player) public view returns (BigNumber memory) {
+    }
+
+    /**
+     * @inheritdoc ICryptolygonIdleV1
+     */
+    function getPlayerLines(address player) public view returns (BigNumber memory) {
+    }
+
+    // Internal functions
+
+    /**
+     * @inheritdoc ICryptolygonIdleV1
+     */
+    function _levelUpPolygon(uint256 polygonId, uint256 amount) internal {
+    }
+
+    /**
+     * @inheritdoc ICryptolygonIdleV1
+     */
+    function _buyUpgrade(uint256 upgradeId, uint256 amount) internal {
+    }
+
+    /**
+     * @inheritdoc ICryptolygonIdleV1
+     */
+    function _buyAscensionPerk(uint256 perkId, uint256 amount) internal {
+    }
+
+    /**
+     * @inheritdoc ICryptolygonIdleV1
+     */
+    function _updatePlayerData() internal {
+    }
+
 }
