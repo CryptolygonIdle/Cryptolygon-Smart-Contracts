4dfe61451027c6f4c5f3a6aa38e012b11f2dfd66
François Liard <56561415+Pacdac@users.noreply.github.com>
2024-02-16 14:19:13 +0100

    Add hardhat plugin contract sizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import '@openzeppelin/hardhat-upgrades';
+import "hardhat-contract-sizer";
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: false,
+    disambiguatePaths: false,
+  },
 };
 
 export default config;
