76b27b374746fd5e07bfa39a6243ff25eb7ac663
François Liard <56561415+Pacdac@users.noreply.github.com>
2024-02-28 12:10:24 +0100

    Add test facets contracts

---
+++ contracts/test/TestFacet1.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract TestFacet1 {
+    function test1() external pure returns (uint256) {
+        return 1;
+    }
+
+    function test2() external pure returns (uint256) {
+        return 3;
+    }
+}
---
+++ contracts/test/TestFacet2.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract TestFacet2 {
+    function test1() external pure returns (uint256) {
+        return 10;
+    }
+
+    function test2() external pure returns (uint256) {
+        return 30;
+    }
+}
